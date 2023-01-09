ec87b94589851780b0be201174555738af4c5056
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2023-01-09 08:32:09 +0500

    add test coverage

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const config = {
+  coverageThreshold: {
+    global: {
+      functions: 100,
+    },
+    // "./src/hooks": {
+    //   statements: 90,
+    // },
+    // "./src/utils": {
+    //   statements: 90,
+    // },
+  },
+  //   collectCoverage: true,
+  //   coverageReporters: ["json", "html"],
+};
+
+module.exports = config;
