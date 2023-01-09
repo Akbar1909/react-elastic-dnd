43184114d2e9e7dd2494e35868013496e1c02ec5
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2023-01-09 11:50:47 +0500

    feat: add useDrag hook

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import useDrag from "./hooks/useDrag";
+import "./styles.css";
 
 const App = () => {
+  const { ref, mode } = useDrag({});
   return (
-    <div>
+    <div
+      style={{
+        background:
+          mode === "dragging" ? "red" : mode === "pressed" ? "yellow" : "blue",
+      }}
+      ref={ref}
+    >
       <h1>Hello World 1234</h1>
       <h2>Welcome to your First React App..!</h2>
     </div>
