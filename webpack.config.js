97f17b5fd2375751fa94c810dc35aa8ff6351bf2
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2023-01-19 09:55:11 +0500

    change webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,7 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+const path = require('path')
 
-const __filename = fileURLToPath(import.meta.url)
-
-const __dirname = path.dirname(__filename)
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js',
